95aae079bfd1708ceb32cb911d3b8a3c73d4be7d
allen0111 <allen.bui.0111@gmail.com>
2016-05-09 17:42:19 -0500

    added comments and moved some stuff around. no change to program flow or structure

--- HttpRequest.java
+++ HttpRequest.java
@@ -1,25 +1,27 @@
-import java.io.* ; 
+import java.io.* ;
 import java.net.* ;
 import java.util.StringTokenizer;
 /**
- * 
- * @author Allen Bui, Sarah Hall (s462b746)
- * Webserver written in Java for Networking Project. Multi-threaded to process more than 1 client request. Prints the request on the server end, serves files on client end along with correct Header   
- * response. 
+ * Webserver written in Java for Networking Project. Multi-threaded to process more than simultaneous requests.
+ * This fulfulls both part 1 and 2 of the networking project. The server side prints the requested line,
+ * and other header-line information as well as serving the file to the client.
+ *
+ * NOTE: this program has been checked and approved by TA abhignan
+ *
+ * @author Allen Bui (x339y958), Sarah Hall (s462b746)
  * @version 1
  */
-
-
 public final class HttpRequest implements Runnable {
+
   	// Continue STAGE 1 from Webserver.java, this is 1 - 3. Structure of HttpRequest class
 	final static String CRLF = "\r\n";
 	Socket socket;
-  
+
 	// Constructor
 	public HttpRequest(Socket socket) throws Exception {
 		this.socket = socket;
 	}
-  
+
 	// Implement the run() method of the Runnable interface.
 	public void run() {
 		try {
@@ -28,68 +30,80 @@ public final class HttpRequest implements Runnable {
 			System.out.println(e);
 		}
 	}
-    
+
 	private void processRequest() throws Exception {
-	// Stage 1 - 4. Creating Socket streams 
+
+		/* * * * * * * * * * * * * * * * * * * * *
+		 * Stage 1 - 4. Creating Socket streams  *
+	 	 * * * * * * * * * * * * * * * * * * * * */
 		// Get a reference to the socket's input and output streams.
 		InputStream is = socket.getInputStream();
 		DataOutputStream os = new DataOutputStream(socket.getOutputStream());
-	    
+
 		// Set up input stream filters.
-		BufferedReader br = new BufferedReader(new InputStreamReader(is)); 
-	// Stage 1 - 5. Reading the Request	  
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+		/* * * * * * * * * * * * * * * * * * *
+		 * Stage 1 - 5. Reading the Request  *
+		 * * * * * * * * * * * * * * * * * * */
 		//Get the request line of the HTTP request message.
 		String requestLine = br.readLine();
-		  
+
 		// Display the request line
 		System.out.println("\n" + requestLine);
-	    
+
 		//Extract the filename from the request line.
 		StringTokenizer tokens = new StringTokenizer(requestLine);
-			
+
 		boolean badRequest = false;
 		boolean methodNotImplemented = false;
-	//Stage 2 - 1. Parse the request    
+
+		/* * * * * * * * * * * * * * * * * *
+		 * Stage 2 - 1. Parse the request  *
+		 * * * * * * * * * * * * * * * * * */
 		String fileName = null;
 		String version = null;
-		
+
 		String command = tokens.nextToken();
-		  
+
 		if (tokens.hasMoreTokens()) {
 			fileName = tokens.nextToken();
 		} else {
 			badRequest = true;
 		}
-		  
+
 		if (tokens.hasMoreTokens()) {
 			version = tokens.nextToken();
 		} else {
 			badRequest = true;
 		}
-	
+
 		if (tokens.hasMoreTokens()) {
 			badRequest = true;
 		}
-	    
+
 		boolean fileExists = true;
 		boolean fileOpen = true;
 		FileInputStream fis = null;
-	    	
-		 
+
+
 		if (!command.equals("GET")) {
 			methodNotImplemented = true;
 		}
-	
+
 		if (!version.matches("HTTP/.[.].")) {
-			System.out.println("Version pattern mismatch");      
+			System.out.println("Version pattern mismatch");
 			badRequest = true;
 		}
-	    
+
 		// Construct the response message.
 		String statusLine = null;
 		String contentTypeLine = null;
 		String entityBody = null;
-	// Stage 2 - 2. Checking if the requested file exists  
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * *
+		 * Stage 2 - 2. Checking if the requested file exists  *
+		 * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 		if (!(badRequest || methodNotImplemented)) {
 			// Open the requested file.
 			try {
@@ -98,7 +112,8 @@ public final class HttpRequest implements Runnable {
 				System.out.println("File not found");
 				fileExists = false;
 			}
-	      		// Construct status line based on ability to open file & file existing
+
+			// Construct status line based on ability to open file & file existing
 			if (fileExists && fileOpen) {
 				statusLine = "HTTP/1.1 200 OK" + CRLF;
 				contentTypeLine = "Content-type: " + contentType(fileName) + CRLF;
@@ -112,20 +127,23 @@ public final class HttpRequest implements Runnable {
 				} else {
 					statusLine = "\nHTTP/1.0 404 Not Found\n" + CRLF;
 					contentTypeLine = "Content-type: " + "text/html" + CRLF;
-					entityBody = "<HTML>" + "<HEAD><TITLE>Not Found</TITLE></HEAD><BODY>File " + fileName + " could not be written</BODY></HTML>" + CRLF; 
+					entityBody = "<HTML>" + "<HEAD><TITLE>Not Found</TITLE></HEAD><BODY>File " + fileName + " could not be written</BODY></HTML>" + CRLF;
 				}
 			}
 		}
-	// Stage 2 - 3 & 5. Creating & Sending the response
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * *
+		 *  Stage 2 - 3 & 5. Creating & Sending the response *
+		 * * * * * * * * * * * * * * * * * * * * * * * * * * */
 		// Send the status line.
 		os.writeBytes(statusLine);
-		  
+
 		// Send the content-type line.
 		os.writeBytes(contentTypeLine);
-		  
+
 		// Send a blank line to indicate the end of the header lines.
 		os.writeBytes(CRLF);
-	
+
 		// Send the entity body.
 		if (!(badRequest || methodNotImplemented) && (fileExists && fileOpen)) {
 			try {
@@ -138,80 +156,84 @@ public final class HttpRequest implements Runnable {
 			 System.out.println(entityBody);
 	    	 os.writeBytes(entityBody);
 		}
-	    
-	// Stage 2 - 6. Closing the Connection 
-	    os.close();
 
-	    // Get and display the header lines.  
-		String headerLine;  
+		/* * * * * * * * * * * * * * * * * * * *
+		 * Stage 2 - 6. Closing the Connection *
+		 * * * * * * * * * * * * * * * * * * * */
+		 os.close();
+
+	  	// Get and display the header lines.
+		String headerLine;
 		while ((headerLine = br.readLine()).length() != 0) {
-			System.out.println(headerLine);  
+			System.out.println(headerLine);
 		}
 
 		br.close();
-	    
-		socket.close();
-  	}
 
-  
+		socket.close();
+	}
+
+
   	private static void sendBytes(FileInputStream fis, OutputStream os) throws Exception {
 
   		// Construct a 1K buffer to hold bytes on their way to the socket.
   		byte[] buffer = new byte[1024];
-    
+
   		int bytes = 0;
-    
+
   		// Copy requested file into the socket's output stream.
   		while ((bytes = fis.read(buffer)) != -1) {
   			os.write(buffer, 0, bytes);
   		}
   	}
-    // Stage 2 - 4. Determining the MIME type
+
+    /* * * * * * * * * * * * * * * * * * * * * *
+     * Stage 2 - 4. Determining the MIME type  *
+     * * * * * * * * * * * * * * * * * * * * * */
     private static String contentType(String fileName) {
-	  
+
     	if (fileName.endsWith(".htm") || fileName.endsWith(".html")) {
-    	
+
     		return "text/html";
-    
+
     	} else if (fileName.endsWith(".jpg") || fileName.endsWith(".jpeg")) {
-    
+
     		return "image/jpeg";
 
     	} else if (fileName.endsWith(".gif")) {
-    
+
     		return "image/gif";
-    
+
     	} else if (fileName.endsWith(".png")) {
-    
+
     		return "image/png";
-    
+
     	} else if (fileName.endsWith(".java")) {
 
     		return "source/java";
-    
+
     	} else if (fileName.endsWith(".py")) {
-    
+
     		return "source/python";
-    
+
     	} else if (fileName.endsWith(".c")) {
-        
+
     		return "source/c";
-    
+
     	} else if (fileName.endsWith(".cpp")) {
-    
+
     		return "source/C++";
-    
+
     	} else if (fileName.endsWith(".txt")) {
-    
+
     		return "text/plain";
 
     	} else if (fileName.endsWith(".pdf")) {
-      
+
     		return "application/pdf";
-    
+
     	}
-   
+
     	return "application/octet-stream";
     }
-  
 }
--- WebServer.java
+++ WebServer.java
@@ -1,41 +1,43 @@
-import java.net.* ; 
+import java.net.* ;
 /**
  * WebServer is a simple networking project used to grasp the concept of TCP/IP programming
- * @author Allen Bui, Sarah Hall
+ * @author Allen Bui (x339y958), Sarah Hall (s462b746)
  * @Version 1
  */
-
-//Begin STAGE 1
 public final class WebServer {
-	
+
 	/**
-	 * main class is the beginning of program execution. 
+	 * main class is the beginning of program execution.
 	 * Establish socket connection and wait for requests.
-	 * @param args N/A in this program 
+	 * @param args N/A in this program
 	 * @throws Exception
 	 */
 	public static void main(String args[]) throws Exception {
 
-		// Stage 1 - 1. Determine the port number
+		/* * * * * * * * * * * * * * * * * * * * * *
+		 * Stage 1 - 1. Determine the port number  *
+		 * * * * * * * * * * * * * * * * * * * * * */
 		int port = 5000;
 
 		// Establish the listen socket.
 		@SuppressWarnings("resource")
 		ServerSocket listenSocket = new ServerSocket(port);
 
-		// Stage 1 - 2. Accepting TCP connection
+		/* * * * * * * * * * * * * * * * * * * * *
+		 * Stage 1 - 2. Accepting TCP connection *
+		 * * * * * * * * * * * * * * * * * * * * */
 		// Process HTTP service requests in an infinite loop
 		while (true) {
-			
+
 			// Listen for a TCP connection request.
 			Socket connectionSocket = listenSocket.accept();
-      
+
 			// Construct an object to process the HTTP request message
 			HttpRequest request = new HttpRequest(connectionSocket);
-      
+
 			//Create a new thread to process the request.
 			Thread thread = new Thread(request);
-      
+
 			// Start the thread.
 			thread.start();
 		}
